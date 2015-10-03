d581817ba38d1260b145da2e102286db68d01937
Andrew Godshalk <andrewgodshalk@gmail.com>
2015-10-03 15:09:17 -0500

    Added code to change transferred configs to output to a uniquely named root file.

--- Ntupler/condor_runDir_redo/collectJobsToReproc.py
+++ Ntupler/condor_runDir_redo/collectJobsToReproc.py
@@ -2,6 +2,34 @@
 from os import system
 from os import listdir
 
+# Function to open a file, find a line starting with something, and replace it with something else.
+def FindLineInFileStartingWithAndReplaceWith(fileName,oldLineBeginning,newLine) :
+    # Open file, read in lines
+    fileToMod = open(fileName, 'r')
+    fileLines = fileToMod.readlines()
+    fileToMod.close()
+    
+    # Find the line in question
+    iToMod = -1
+    for line in fileLines:
+        if line.startswith(oldLinePart) :
+           iToMod = fileLines.index(line)
+    
+    # Change line to appropriate True/False
+    fileLines[iToMod] = newLine
+    
+    # Read all lines out to a new file.
+    fileToMod = open(fileName, 'w')
+    for line in fileLines :
+        fileToMod.write(line)
+    fileToMod.close()
+
+
+
+
+
+
+
 # Strings to find
 THE_STRINGS = [
   "Aborted                 ./Ntupler ntuple_for_condor.py",
@@ -25,12 +53,15 @@ for ds in datasets:
     fileList = [ item for item in fileList if item.endswith(".stderr")]
   # If the file contains THE STRING...
     for errFile in fileList :
-	for THE_STRING in THE_STRINGS :
+        for THE_STRING in THE_STRINGS :
             if THE_STRING in open(dirName+"/"+errFile).read() :
               # Save the corresponding config file to a new folder.
                 jobNumber = errFile.split('_')[2].split('.')[0]
-                system("cp "+dirName+"/ntuple_job_"+jobNumber+".py ntuple_job_"+ds+"_"+jobNumber+".py")
+                newFileName = "ntuple_job_"+ds+"_"+jobNumber+".py"
+                system("cp "+dirName+"/ntuple_job_"+jobNumber+".py "+newFileName)
                 #system("cp "+dirName+"/"+errFile+" "+ds+"_"+errFile)
+              # Change the output of the config file to something similar
+                FindLineInFileStartingWithAndReplaceWith(newFileName, "fname = 'Test' + channel +", "fname = 'Test' + channel + '_"+ds+"_"+jobNumber+".root'\n")
                 break
 
 # Get list of config files to redo
@@ -52,23 +83,5 @@ system("rm ntuple_job_*")
 fileName = "condor_config.script"
 oldLinePart = "Queue"
 newLine = "Queue "+str(len(fileList))+"\n"
+FindLineInFileStartingWithAndReplaceWith(fileName,oldLinePart,newLine)
 
-# Open file, read in lines
-fileToMod = open(fileName, 'r')
-fileLines = fileToMod.readlines()
-fileToMod.close()
-
-# Find the line in question
-iToMod = -1
-for line in fileLines:
-    if line.startswith(oldLinePart) :
-        iToMod = fileLines.index(line)
-
-# Change line to appropriate True/False
-fileLines[iToMod] = newLine
-
-# Read all lines out to a new file.
-fileToMod = open(fileName, 'w')
-for line in fileLines :
-    fileToMod.write(line)
-fileToMod.close()
