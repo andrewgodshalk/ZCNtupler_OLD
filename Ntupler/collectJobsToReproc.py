92901dc029e8867d8ac939e17b96c0b7286d84f7
Andrew Godshalk <andrewgodshalk@gmail.com>
2015-10-02 21:52:08 -0500

    Completed script to gather config files for reproc.

--- Ntupler/collectJobsToReproc.py
+++ Ntupler/collectJobsToReproc.py
@@ -1,6 +1,8 @@
 
-# String to find:  "Aborted                 ./Ntupler ntuple_for_condor.py"
+from os import system
 
+# String to find
+THE_STRING = "Aborted                 ./Ntupler ntuple_for_condor.py"
 
 # Get list of datasets
 countFile = open('formattedFileLists/file_counts_per_list.txt', 'r')
@@ -8,11 +10,18 @@ datasets = []
 for line in countFile :
     datasets.append(line.split()[0])
 
+system("mkdir condor_runDir_do-over")
+
 # In each direcotry...
 for ds in datasets:
   # Check each stderr for THE STRING
-    
-  # If they contain the string...
-    # Save the corresponding config file to a new folder.
-
-
+    dirName = "condor_runDir_"+ds
+  # Create a list of stderr files
+    fileList = listdir(dirName)
+    fileList = [ item for item in fileList if item.endswith(".stderr")]
+  # If the file contains THE STRING...
+    for errFile in fileList :
+        if THE_STRING in open(errFile).read() :
+          # Save the corresponding config file to a new folder.
+            jobNumber = errFile.split('_')[2].split('.')[0]
+            system("cp "+dirName+"/ntuple_job_"+jobNumber+".py condor_runDir_do-over/")
